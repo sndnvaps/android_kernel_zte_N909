186904b0e916556caae678a44a182530ea6eea93
Mkey_NJU <499238317@qq.com>
2014-04-20 22:27:09 +0800

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sun Apr 20 18:39:45 CST 2014"
+#define LINUX_COMPILE_BY "mkey"
+#define LINUX_COMPILE_HOST "mkey-Inspiron-N4010"
+#define LINUX_COMPILER "gcc version 4.8.1 (Sourcery CodeBench Lite 2013.11-33) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-mkey-Inspiron-N4010"
